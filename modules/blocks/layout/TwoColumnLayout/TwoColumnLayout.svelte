128d5443ff516d89a987946f1f4eaa7c501cbaf0
Joshua Harris <joshua@rescuingleftovercuisine.org>
2024-11-07 20:54:15 -0500

    Two Column Layout: Vertically center items.

--- modules/blocks/layout/TwoColumnLayout/TwoColumnLayout.svelte
+++ modules/blocks/layout/TwoColumnLayout/TwoColumnLayout.svelte
@@ -8,7 +8,7 @@
 <div class='rlc__block rlc__block--layout rlc__block--layout--theme-{ data.theme } rlc__block__two-column-layout'>
   <div
     style='grid-template-columns: { gridColumnRatioLeft }fr { gridColumnRatioRight }fr;'
-    class='rlc__block__two-column-layout__content tablet:rlc-grid'
+    class='rlc__block__two-column-layout__content tablet:rlc-grid tablet:rlc-items-center'
   >
     <slot name='column-left' />
     <slot name='column-right' />
--- modules/blocks/layout/TwoColumnLayout/TwoColumnLayoutExample.svelte
+++ modules/blocks/layout/TwoColumnLayout/TwoColumnLayoutExample.svelte
@@ -7,28 +7,16 @@
   import { twoColumnLayoutBaseData } from './TwoColumnLayout.data.js'
   const twoColumnLayoutData = mergeDataForStory(twoColumnLayoutBaseData, data)
 
-  import Copy from '@Content/Copy/Copy.svelte'
-  import { copyWithAllSupportedElements } from '@Content/Copy/Copy.data.js'
-  const copyData = mergeDataForStory(copyWithAllSupportedElements, {})
-
   import Image from '@Content/Image/Image.svelte'
   import { imageBaseData } from '@Content/Image/Image.data.js'
   const imageData = mergeDataForStory(imageBaseData)
 
-  import Button from '@Content/Button/Button.svelte'
-  import { buttonBaseData } from '@Content/Button/Button.data.js'
-  const buttonData = mergeDataForStory(buttonBaseData)
-
   import LeadCaptureForm from '@Content/LeadCaptureForm/LeadCaptureForm.svelte'
   import { leadCaptureFormBaseData } from '@Content/LeadCaptureForm/LeadCaptureForm.data.js'
   const leadCaptureFormData = mergeDataForStory(leadCaptureFormBaseData)
-
-  import Video from '@Content/Video/Video.svelte'
-  import { videoBaseData } from '@Content/Video/Video.data.js'
-  const videoData = mergeDataForStory(videoBaseData)
 </script>
 
 <TwoColumnLayout { ...twoColumnLayoutData }>
   <Image slot='column-left' { ...imageData } />
-  <Copy slot='column-right' { ...copyData } />
+  <LeadCaptureForm slot='column-right' { ...leadCaptureFormBaseData } />
 </TwoColumnLayout>
